54f796d67bbd9d914e149ff58cd7ff6ada94bf76
Latoya Williams <70241027+LatoyaWill@users.noreply.github.com>
2022-07-19 21:44:58 -0500

    Update TestController.java

--- src/main/java/training/com/usk/controller/TestController.java
+++ src/main/java/training/com/usk/controller/TestController.java
@@ -3,7 +3,11 @@ package training.com.usk.controller;
 public class TestController {
 
 	public static void main(String[] args) {
+<<<<<<< Updated upstream
 		// TODO Auto-generated method stub
+=======
+		// dev1 comments added
+>>>>>>> Stashed changes
 
 	}
 

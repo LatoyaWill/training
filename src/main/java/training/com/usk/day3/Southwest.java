0db586a0ae189d20661ea41051e75fee87e8f2f3
Latoya Williams <70241027+LatoyaWill@users.noreply.github.com>
2022-07-21 17:28:14 -0500

    Inheritance Assignment

---
+++ src/main/java/training/com/usk/day3/Southwest.java
@@ -0,0 +1,19 @@
+package training.com.usk.day3;
+
+public class Southwest extends Airline {
+	
+	//Child class
+	
+	private String boardingClass;
+	
+	public void getBoardingClass() {
+		System.out.println(boardingClass);
+	}
+
+	public void setBoardingClass(String boardingClass) {
+		this.boardingClass = boardingClass;
+	}
+	
+	
+			
+}
